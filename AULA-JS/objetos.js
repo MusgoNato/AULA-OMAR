586ab87eb868c0bb1623d6cbf41dc86d720d32ad
Hugo Josué lema das neves <131496781+MusgoNato@users.noreply.github.com>
2024-06-21 20:57:53 -0400

    commit
    asd

---
+++ AULA-JS/objetos.js
@@ -0,0 +1,14 @@
+/*Strings*/
+/*Esse cara cria um objeto do tipo string, precisa ser string para poder pegar qualquer coisa*/
+var variavel = new String();
+
+/*A varaivel so vai ser de algum tipo quando for atribuida, nesse caso vai ser do tipo string, pois atribuo uma string dentro da variavel asd*/
+var asd = 'paocommortadela'; 
+
+/*Console,log mostra na tela, no caso de debugação eh bastante util*/
+console.log(variavel);
+console.log(variavel.length);
+
+/*Typeof pega o tipo da variavel dentro passada como argumento para ele*/
+console.log(typeof(variavel));
+console.log(typeof(asd));
